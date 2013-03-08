29b5a1e4859ea81d4fea241f68a6fa3ad30b1dbd
r2fresh <r2fresh78@gmail.com>
2013-03-08 17:53:04 +0900

    dailJS step1 따라하는중

---
+++ dailyJS/server.js
@@ -0,0 +1,10 @@
+var connect = require('connect'), http = require('http'), app;
+
+app = connect()
+.use(connect.static('app'))
+.use('/js/lib/', connect.static('node_modules/requirejs'))
+.use('/node_modules', connect.static('node_modules'));
+
+http.createServer(app).listen(8082, function(){
+	console.log('Running on http://localhost:8082');
+})
